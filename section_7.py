b0df20fe4d09e149d157714f4c51406d661cc269
hotzjacobb <hotzjacobb@gmail.com>
2020-06-28 14:35:57 -0700

    crossed validation with varying number of folds

--- section_7.py
+++ section_7.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor
 import sklearn.metrics as metrics
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 
 def price_tofloat(price): # takes price as a string and outputs as a float
     price_to_convert = price.replace('$', '')
@@ -12,12 +14,33 @@ def price_tofloat(price): # takes price as a string and outputs as a float
         exit(-1)
 
 
+def k_fold(dataset, k):
+    list_of_rsme = [] # result
+    model = KNeighborsRegressor()
+    partition_size = len(dataset) / k
+    dataset['fold'] = dataset.index.values / partition_size
+    dataset['fold'].astype('int')
+    for current_test in range(k):
+        training_data = dataset[dataset['fold'] != current_test]
+        testing_data = dataset[dataset['fold'] == current_test]
+        model.fit(training_data[['accommodates']], training_data[['price']])
+        predicted_prices = model.predict(testing_data[['accommodates']])
+        rsme = np.sqrt(metrics.mean_squared_error(testing_data['price'], predicted_prices))
+        list_of_rsme.append(rsme) 
+    return list_of_rsme
+
+
 
 airbnb = pd.read_csv('paris_airbnb.csv')
 airbnb['price'] = airbnb['price'].apply(price_tofloat) # change prices to floats
 np.random.seed(seed=1)
-permutation = np.random.permutation(len(airbnb))
-airbnb.reindex(permutation)
+
+permutation = np.random.permutation(airbnb.index)
+airbnb = airbnb.reindex(permutation)
+
+list_of_results = k_fold(airbnb, 5)
+avg_rsme = np.mean(list_of_results)
+
 split_one = airbnb.iloc[0:4000]
 split_two = airbnb.iloc[4000:8000]
 
@@ -33,7 +56,29 @@ second_preds = knn.predict(split_one[['accommodates']]) # preds. for second spli
 second_rmse = np.sqrt(metrics.mean_squared_error(split_one[['price']], second_preds))
 
 mean = np.mean([first_preds, second_preds])
-print(mean)
+
+
+# add values to rows for k-fold
+airbnb['fold'] = airbnb.index.values / (len(airbnb) / 5)
+airbnb['fold'] = airbnb['fold'].astype('int')
+
+
+folds_model = KNeighborsRegressor()
+test_part = airbnb[airbnb['fold'] == 0]
+training_part = airbnb[airbnb['fold'] != 0]
+folds_model.fit(training_part[['accommodates']], training_part[['price']])
+predicted_prices = folds_model.predict(test_part[['accommodates']])
+rsme = np.sqrt(metrics.mean_squared_error(test_part['price'], predicted_prices))
+
+# Use library functions to perform k-fold
+kf = KFold(shuffle=True, random_state=1)
+knn = KNeighborsRegressor()
+results = cross_val_score(knn, airbnb[['accommodates']], 
+                          airbnb['price'], scoring='neg_mean_squared_error', cv=kf)
+results = np.sqrt(np.absolute(results))
+result = np.mean(results)
+print(results)
+print(result)
 
 
 
